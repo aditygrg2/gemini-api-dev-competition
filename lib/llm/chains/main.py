07e9e611534dbfd7e6928b5098a80aaf5a02adcc
Aditya Garg <uber6707@gmail.com>
2024-06-17 17:15:23 +0530

    repository cleanup

--- lib/llm/chains/main.py
+++
@@ -1,9 +0,0 @@
-from database.actions.main import Database
-
-class ConversationalChain():
-    def __init__(self, mobileNumber: str) -> None:
-        db = Database()
-        self.userDetails = db.getUserDetails(mobileNumber)
-        pass
-    
-    
